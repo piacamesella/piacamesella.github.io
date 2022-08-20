1ffc1d26a0fc00539731aee0aba2f8f71742e677
piacamesella <pia.camesella@gmail.com>
2022-08-20 04:33:50 -0300

    loginnnn

--- js/login.js
+++ js/login.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("submitButton").addEventListener("click",function(){
+        let email=document.getElementById("inputEmail").value;
+        let password=document.getElementById("inputPassword").value;
         if (email!== "" && password!==""){
             window.location.replace("https://github.com/piacamesella/piacamesella.github.io/blob/main/portada.html");
         }})
     })
-    let email=document.getElementById("inputEmail").value;
-    let password=document.getElementById("inputPassword").value;
+
     
