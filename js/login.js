a4e931a7def667a4fda4698eaa0510e636bc0817
piacamesella <pia.camesella@gmail.com>
2022-08-20 01:51:20 -0300

    login

---
+++ js/login.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("submitBtn").addEventListener("click",function(){
+        let email=document.getElementById("inputEmail").value;
+    })
+})
+if(email!==""&&password)
+window.location.replace("portada.html");
