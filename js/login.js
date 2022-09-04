1b345e3e59b3055460b2f8312de10774309cf028
piacamesella <pia.camesella@gmail.com>
2022-09-04 02:48:56 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,8 +1,11 @@
+
 document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("submitButton").addEventListener("click",function(){
         let email=document.getElementById("inputEmail").value;
         let password=document.getElementById("inputPassword").value;
         if (email!== "" && password!==""){
+            console.log(localStorage.setItem);
+            localStorage.setItem("usuario",email);
             window.location.replace("portada.html");
         }})
     })
