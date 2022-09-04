cb8340f0da66edd30c7799648bd7204b9bf83ff9
piacamesella <pia.camesella@gmail.com>
2022-09-04 05:50:35 -0300

    jkl

--- js/init.js
+++ js/init.js
@@ -9,6 +9,9 @@ const CART_INFO_URL = "https://japceibal.github.io/emercado-api/user_cart/";
 const CART_BUY_URL = "https://japceibal.github.io/emercado-api/cart/buy.json";
 const EXT_TYPE = ".json";
 const productos_todos= `PRODUCTS_URL${localStorage.getItem("catID")}EXT_TYPE`;
+console.log(productos_todos);
+console.log(EXT_TYPE);
+console.log(PRODUCTS_URL);
 
 
 let showSpinner = function(){
