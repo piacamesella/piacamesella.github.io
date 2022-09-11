f6a527a05131282c8e988dee7f896ca4f628eed1
piacamesella <pia.camesella@gmail.com>
2022-09-11 04:17:06 -0300

    jkl

--- js/products.js
+++ js/products.js
@@ -135,7 +135,7 @@ document.getElementById("filtrar").addEventListener("click", function(){
     }
     
     if (document.getElementById("rango-max")!=""){
-        max = document.getElementById("rango-max").value
+        max = document.getElementById("rango-max").valuen
     }else{
         max = undefined;
     }
