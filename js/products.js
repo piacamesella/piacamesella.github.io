4cfe086ecb6c3720641588383cd1e22f7d32c247
piacamesella <pia.camesella@gmail.com>
2022-09-11 04:15:10 -0300

    jl

--- js/products.js
+++ js/products.js
@@ -129,13 +129,13 @@ document.getElementById("filtrar").addEventListener("click", function(){
 
     //Obtengo el mínimo y máximo para filtrar por precios
     if (document.getElementById("rango-min") !=""){
-        min = document.getElementById("rango-min");
+        min = document.getElementById("rango-min").value
     }else{
         min = undefined;
     }
     
     if (document.getElementById("rango-max")!=""){
-        max = document.getElementById("rango-max");
+        max = document.getElementById("rango-max").value
     }else{
         max = undefined;
     }
