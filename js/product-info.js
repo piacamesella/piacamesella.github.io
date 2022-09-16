502de32f986c26a3acce87c496376dd7e63aad4a
piacamesella <pia.camesella@gmail.com>
2022-09-16 03:58:13 -0300

    k

--- js/product-info.js
+++ js/product-info.js
@@ -1,1 +1,12 @@
+const PRODUCTO_INFO =`https://japceibal.github.io/emercado-api/products/${localStorage.getItem("productID")}.json`;
 localStorage.getItem("productID");
+document.addEventListener("DOMContentLoaded", function(a){
+    getJSONData(PRODUCTO_INFO).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            arrayProducts=resultObj.data.products;
+            mostrarArrayProducts(arrayProducts);
+            console.log(arrayProducts);
+        }
+    });
+
