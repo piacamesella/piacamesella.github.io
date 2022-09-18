d735f64d94e3f8e9a77301029eb2c8826947c438
piacamesella <pia.camesella@gmail.com>
2022-09-17 22:12:36 -0300

    jlj

--- js/product-info.js
+++ js/product-info.js
@@ -1,27 +1,28 @@
 arrayProducts = [];
 function mostrarArrayProducts(){
 
-    let htmlContentToAppend = "";
-            htmlContentToAppend += `
-            <div onclick="setProductID(${products.id})" class="list-group-item list-group-item-action cursor-active">
-                <div class="row">
-                    <div class="col-3">
-                        <img src="${products.image}" alt="${products.description}" class="img-thumbnail">
+        let htmlContentToAppend = "";
+        htmlContentToAppend += `
+        <div onclick="setProductID(${products.id})" class="list-group-item list-group-item-action cursor-active">
+            <div class="row">
+                <div class="col-3">
+                    <h4 <small class="text-muted">${products.name} </small>>
+                </div>
+                <p class="mb-1">${products.cost}</p> 
+                <p "${products.description}" >
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">${products.category}</h4>
+                        <p ${products.soldCount}>
+                        <small class="text-muted">${products.soldCount} artículos</small>
                     </div>
-                    <div class="col">
-                        <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">${products.name} - ${products.currency} ${products.cost}</h4>
-                            <small class="text-muted">${products.soldCount} artículos</small>
-                        </div>
-                        <p class="mb-1">${products.description}</p>
-                    </div>
+                    <img src="${products.image}" class="img-thumbnail">
                 </div>
             </div>
-            `
-        }
-        document.getElementById("productsId").innerHTML = htmlContentToAppend;
-     }
-  }
+        </div>
+        `
+    }
+    document.getElementById("productsId").innerHTML = htmlContentToAppend;
 
 let productInfo;
 
