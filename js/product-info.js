2ba2f3507d35001c3a976c4f4a65314ea7c915e3
piacamesella <pia.camesella@gmail.com>
2022-09-18 18:44:00 -0300

    nk

--- js/product-info.js
+++ js/product-info.js
@@ -35,7 +35,7 @@ document.addEventListener("DOMContentLoaded", function(a){
         }
     });
 
-localStorage.getItem("productID").addEventListener("click", function(){
+    document.getElementById(localStorage.getItem("productID").valueOf).addEventListener("click", function(){
     productInfo = localStorage.getItem("productInfo");
     mostrarInfoProducts();
 });
