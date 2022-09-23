3889854b6be77c2f681a330ad1db6a3995bb44ed
piacamesella <pia.camesella@gmail.com>
2022-09-23 05:15:27 -0300

    hlk

--- js/product-info.js
+++ js/product-info.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function(a){
         {
             productInfo=resultado.data;
             document.getElementById("contenedor").innerHTML += `
-                <"(${resultado.data.id})" class="list-group-item list-group-item-action cursor-active">
+                <"(${resultado.data.id})" class="list-group-item">
                 <div class="row">
                     <div class="col-3">
                         <h4 <small class="text">${resultado.data.name} </small>>
