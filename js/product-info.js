73d823521c2229c4b0ab2b7c17aad8c035ec387b
piacamesella <pia.camesella@gmail.com>
2022-09-23 02:58:59 -0300

    hgkuj

--- js/product-info.js
+++ js/product-info.js
@@ -4,20 +4,20 @@ function mostrarInfoProducts(){
 
         let htmlContentToAppend = "";
         htmlContentToAppend += `
-        <div onclick="setProductID(${productInfo.id})" class="list-group-item list-group-item-action cursor-active">
+        <div onclick="setProductID(${products.id})" class="list-group-item list-group-item-action cursor-active">
             <div class="row">
                 <div class="col-3">
                     <h4 <small class="text-muted">${resultado.data.name} </small>>
                 </div>
-                <p class="mb-1">${productInfo.cost}</p> 
-                <p "${productInfo.description}" >
+                <p class="mb-1">${products.cost}</p> 
+                <p "${products.description}" >
                 <div class="col">
                     <div class="d-flex w-100 justify-content-between">
-                        <h4 class="mb-1">${productInfo.category}</h4>
-                        <p ${productInfo.soldCount}>
-                        <small class="text-muted">${productInfo.soldCount} artículos</small>
+                        <h4 class="mb-1">${products.category}</h4>
+                        <p ${products.soldCount}>
+                        <small class="text-muted">${products.soldCount} artículos</small>
                     </div>
-                    <img src="${productInfo[0].image}" class="img-thumbnail">
+                    <img src="${products[0].image}" class="img-thumbnail">
                 </div>
             </div>
         </div>
@@ -32,20 +32,20 @@ document.addEventListener("DOMContentLoaded", function(a){
         {
             productInfo=resultado.data;
             document.getElementById("contenedor").innerHTML += `
-                <   <div onclick="setProductID(${productInfo.id})" class="list-group-item list-group-item-action cursor-active">
+                <   <div onclick="setProductID(${products.id})" class="list-group-item list-group-item-action cursor-active">
                 <div class="row">
                     <div class="col-3">
                         <h4 <small class="text-muted">${resultado.data.name} </small>>
                     </div>
-                    <p class="mb-1">${productInfo.cost}</p> 
-                    <p "${productInfo.description}" >
+                    <p class="mb-1">${products.cost}</p> 
+                    <p "${products.description}" >
                     <div class="col">
                         <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">${productInfo[productInfo.findIndex(prinf => productInfo.id = localStorage.getItem("productID"))].category}</h4>
-                            <p ${productInfo.soldCount}>
-                            <small class="text-muted">${productInfo.soldCount} artículos</small>
+                            <h4 class="mb-1">${products[products.findIndex(prinf => products.id = localStorage.getItem("productID"))].category}</h4>
+                            <p ${products.soldCount}>
+                            <small class="text-muted">${products.soldCount} artículos</small>
                         </div>
-                        <img src="${productInfo.image}" class="img-thumbnail">
+                        <img src="${products.image}" class="img-thumbnail">
                     </div>
                 </div>
             </div>
