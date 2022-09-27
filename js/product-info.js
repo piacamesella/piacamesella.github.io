a87f4f05168d5df41548ea22f65e660a2cdb3c2e
piacamesella <pia.camesella@gmail.com>
2022-09-27 05:32:53 -0300

    jkl

--- js/product-info.js
+++ js/product-info.js
@@ -61,11 +61,13 @@ document.addEventListener("DOMContentLoaded", function(){
             let products = productInfo[i];
             
                 htmlContentToAppend += `
-                <div (${products.description}) class="list-comments-item">
+                <div  class="list-comments-item">
                     <div class="row">
                         <div class="col">
                             <div class="d-flex w-100 justify-content-between">
-                                <h4 class="mb-1">${products.user} - ${products.dateTime} ${estrellas(products.score)}</h4>
+                                <h4 class="mb-1">${products.user} - ${products.dateTime} ${estrellas(products.score)}</h4><br
+                        </div>
+                        <div <small class="text">(${products.description})</small>>
                         </div>
                     </div>
                 </div>
