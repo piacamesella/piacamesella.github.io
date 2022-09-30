1f347d4139b7dd98d31926a7f3e41d6b6e93c8d7
piacamesella <pia.camesella@gmail.com>
2022-09-30 03:48:51 -0300

    l

--- js/product-info.js
+++ js/product-info.js
@@ -78,7 +78,7 @@ document.addEventListener("DOMContentLoaded", function(){
                             <div class="d-flex w-100 justify-content-between">
                             <h4 class="mb-1">${products.user} - <small class="text-muted">${products.dateTime}</small> ${estrellas(products.score)}</h4><br>
                         <div>
-                            <small class="text-muted">${products.description}</small>
+                            <br><small class="text-muted">${products.description}</small>
                         </div>
                     </div>
                 </div>
