905643f907564d841df335faf4a46aa3fe2f8f49
piacamesella <pia.camesella@gmail.com>
2022-09-30 04:07:55 -0300

    b

--- js/product-info.js
+++ js/product-info.js
@@ -14,17 +14,6 @@ function getProductID(id){
     localStorage.getItem("productID",id);
 }
 
-function estrellas(score){
-    for(let i = 0; i < 5; i++){
-        let estrellasScore = score[i];
-        console.log(estrellasScore);
-        document.getElementById("estrellasDoradas").innerHTML = estrellasDoradas;
-        estrellasScore=5-score;
-        console.log(estrellasScore);
-        document.getElementById("estrellasNegras").innerHTML = estrellasNegras;
-        }
-}    
-
 function mostrarProductInfo(){
     htmlContentToAppend+= `
      <div onclick="getProductID(${"productID"})" class="list-group-item">
@@ -97,7 +86,16 @@ document.addEventListener("DOMContentLoaded", function(){
          }
       }
       );
-      
+      function estrellas(score){
+        for(let i = 0; i < 5; i++){
+            let estrellasScore = score[i];
+            console.log(estrellasScore);
+            document.getElementById("estrellasDoradas").innerHTML = estrellasDoradas;
+            estrellasScore=5-score;
+            console.log(estrellasScore);
+            document.getElementById("estrellasNegras").innerHTML = estrellasNegras;
+            }
+    }        
 });
 
 
