c0a12c41ad8066663bd84207e0a70fa5c208bead
piacamesella <pia.camesella@gmail.com>
2022-10-15 22:03:20 -0300

    k

--- js/cart.js
+++ js/cart.js
@@ -45,7 +45,7 @@ function mostrarCarritoInfo(){
 
                     </div>
                     <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                      <h6 class="mb-0">${getUserID("userID").articles.currency} ${articles.unitCost}</h6>
+                      <h6 class="mb-0">${getUserID("userID").articles.currency} ${user.articles.unitCost}</h6>
                     </div>
                     <div class="col-md-1 col-lg-1 col-xl-1 text-end">
                       <a href="#!" class="text-muted"><i class="fas fa-times"></i></a>
