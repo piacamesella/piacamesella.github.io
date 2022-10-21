657f70ed973bd638a14a99c600877a9946561cf8
piacamesella <pia.camesella@gmail.com>
2022-10-21 02:49:32 -0300

    K

--- js/cart.js
+++ js/cart.js
@@ -151,7 +151,7 @@ function mostrarCarritoInfo(){
                   <hr class="my-4">
 
                   <div class="pt-5">
-                    <h6 class="mb-0"><a href="#!" class="text-body"><i
+                    <h6 class="mb-0"><a href="${PRODUCTOS_TODOS}" class="text-body"><i
                           class="fas fa-long-arrow-alt-left me-2"></i>Back to shop</a></h6>
                   </div>
                 </div>
