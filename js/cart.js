0061101d1b2fe22136d11d2444ed9f8af5562ea5
piacamesella <pia.camesella@gmail.com>
2022-10-20 21:17:10 -0300

    k

--- js/cart.js
+++ js/cart.js
@@ -126,7 +126,7 @@ function mostrarCarritoInfo(){
            <td><h2 <small class="text">${articles.name}</small></td>
            <td> <small class="text-muted">${articles.currency} ${articles.unitCost}</small></td>
            <td><input id="form1" min="0" name="quantity" value="${articles.count}" type="number"
-                 class="form-control form-control-sm" /></td>
+                 class="form-control form-control-sm"/></td>
            <td><small class="text-muted">${articles.count*articles.unitCost}</small></td>
          </tr>
        </tbody>
