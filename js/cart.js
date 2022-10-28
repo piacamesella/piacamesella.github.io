cc001a4ba2932f18c02f0c2ccb40284b89443473
piacamesella <pia.camesella@gmail.com>
2022-10-28 03:08:45 -0300

    k

--- js/cart.js
+++ js/cart.js
@@ -26,7 +26,7 @@ function subtotal(cantidad,precio){
     cantidad=document.getElementById("form1").value;
     subtotalResultado=cantidad*precio
     document.getElementById("subtotalID").innerHTML=`
-    <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
+    <div class="col-md-3 col-lg-6 col-xl-2 offset-lg-1">
       <h6 class="mb-4">${subtotalResultado}</h6>
     </div>
     `
