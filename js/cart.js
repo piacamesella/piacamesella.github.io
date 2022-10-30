242c75baa01c173571b19880cd9c16fbd216e541
piacamesella <pia.camesella@gmail.com>
2022-10-30 20:00:59 -0300

    k

--- js/cart.js
+++ js/cart.js
@@ -41,19 +41,19 @@ function mostrarCarritoInfo(){
                   <hr class="my-4">
 
                   <div class="row mb-4 d-flex justify-content-between align-items-center">
-                    <div class="col-md-2 col-lg-2 col-xl-2">
+                    <div class="col-xs-2 col-md-2 col-lg-2 col-xl-2">
                       <img src="${carritoInfo.articles[i].image}"class="img-thumbnail"></img>
                     </div>
-                    <div class="col-md-3 col-lg-3 col-xl-3">
+                    <div class="col-xs-3 col-md-3 col-lg-3 col-xl-3">
                       <h6 <small class="text">${carritoInfo.articles[i].name}</small></h6>
                     </div>
-                    <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
+                    <div class="col-xs-3 col-md-3 col-lg-3 col-xl-2 d-flex">
 
                       <input id="form1" min="0" name="quantity" value="${carritoInfo.articles[i].count}" type="number" oninput="subtotal(${carritoInfo.articles[i].count},${carritoInfo.articles[i].unitCost})"
                         class="form-control form-control-sm"/>
 
                     </div>
-                    <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1" id="subtotalID">
+                    <div class="col-xs-2 col-md-3 col-lg-2 col-xl-2 offset-lg-1" id="subtotalID">
                       <h6 class="mb-0">${carritoInfo.articles[i].currency} ${carritoInfo.articles[i].unitCost}</h6>
                     </div>
                   </div>
