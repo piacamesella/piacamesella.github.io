3d583ff7afc6df0d554c2c1c27210f4529a007f1
piacamesella <pia.camesella@gmail.com>
2022-11-01 21:02:42 -0300

    ;

--- js/cart.js
+++ js/cart.js
@@ -68,8 +68,14 @@ function mostrarCarritoInfo(){
               </div>
               <div class="col-lg-4 bg-mostaza">
                 <div class="p-5">
-                
+                  <h3 class="fw-bold mb-5 mt-2 pt-1">Costos</h3>
+                  <hr class="my-4">
 
+                  <div class="d-flex justify-content-between mb-4" id="subtotalID">
+                    <h5 class="text-uppercase">Subtotal</h5>
+                    <h5>${carritoInfo.articles[i].currency} ${carritoInfo.articles[i].count*carritoInfo.articles[i].unitCost}</h5>
+                  </div>
+                
                   <h5 class="text-uppercase mb-3">Tipo de envío</h5>
 
                   <div class="mb-4 pb-2">
@@ -112,6 +118,12 @@ function mostrarCarritoInfo(){
                     </form>
                   </div>
 
+                  <hr class="my-4">
+
+                  <div class="d-flex justify-content-between mb-5">
+                    <h5 class="text-uppercase">Total</h5>
+                    <h5>AgregarCalculoTotal</h5>
+                  </div>
 
                   <hr class="my-4">
 
