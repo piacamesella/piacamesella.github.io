77aefd13f4feb973229a4766295828b8f36738cb
piacamesella <pia.camesella@gmail.com>
2022-11-23 02:32:35 -0300

    j

--- js/init.js
+++ js/init.js
@@ -46,21 +46,3 @@ let getJSONData = function(url){
         return result;
     });
 }
-
-document.addEventListener("DOMContentLoaded", function(){
-  let email=localStorage.getItem("usuario");
-  document.getElementById("emailIngresado").innerHTML= email;
-  document.getElementById("miCarrito").addEventListener("click", function() {
-      window.location = "cart.html"
-  });
-  document.getElementById("miPerfil").addEventListener("click", function() {
-      if (localStorage.gettItem("usuario",email).value!==""){
-          window.location = "my-profile.html" 
-      }
-  });
-  document.getElementById("cerrarSesion").addEventListener("click", function() {
-      localStorage.removeItem("usuario");
-      window.location = "index.html"
-  });
-  
-});
