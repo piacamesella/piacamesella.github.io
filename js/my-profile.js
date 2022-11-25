3bc9bb6ea4c843297ae68697b3b64f354ac5e639
piacamesella <pia.camesella@gmail.com>
2022-11-25 07:09:14 -0300

    j

--- js/my-profile.js
+++ js/my-profile.js
@@ -5,12 +5,55 @@ let inputSegundoNombre=document.getElementById("segundoNombre");
 let inputApellido= document.getElementById("primerApellido");
 let inputSegundoApellido= document.getElementById("segundoApellido");
 let inputTel=document.getElementById("numeroTel");
+let datoNombre="";
+let datoSegundoNombre="";
+let datoApellido="";
+let datoSegundoApellido="";
+let datoTel="";
+
+// function nombreIngresado(){
+//     datoNombre=inputNombre.value;
+// }
+
+// function segundoNombreIngresado(){
+//     datoSegundoNombre=inputSegundoNombre.value;
+// }
+
+// function apellidoIngresado(){
+//     datoApellido=inputApellido.value;
+// }
+
+// function segundoApellidoIngresado(){
+//     datoSegundoApellido=inputSegundoApellido.value;
+// }
+
+// function telIngresado(){
+//     datoTel=inputTel.value;
+// }
+
 
 // document.addEventListener("DOMContentLoaded",function(){
-//   if(){
+//   if(localStorage.getItem("datosPerfil")=""){
 //     inputEmail.value=email && inputNombre.value=="" && inputSegundoNombre.value=="" && inputApellido.value=="" && inputSegundoApellido.value=="" && inputTel.value=="";
 //   }
 // })
+
+
+document.getElementById("guardarCambiosID").addEventListener("click", function(){
+    if(inputNombre.value!=="" && inputApellido.value!=="" && inputTel.value!==""){
+        let datos={
+            nombre:inputNombre.value,
+            segundoNombre: inputSegundoNombre.value,
+            apellido: inputApellido.value,
+            segundoApellido:inputSegundoApellido.value,
+            emailLogin: email,
+            tel:inputTel.value,
+        };
+        localStorage.setItem("datosPerfil",datos);
+        console.log(datos.nombre);
+    }
+})
+
 document.getElementById("emailID").value=email;
 
 document.addEventListener("DOMContentLoaded", function(){
